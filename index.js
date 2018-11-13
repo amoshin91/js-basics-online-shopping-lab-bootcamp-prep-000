b1929f60be377e60b860dd305e9956eca4ef16bd
Yongwoo Shin <amoshinihsoma@gmail.com>
2018-11-13 20:34:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function addToCart(item) {
 
 function viewCart() {
   if (cart.length === 0) {
-    return "Your shopping cart is empty"
+    return "Your shopping cart is empty."
     
   } else if (cart.length === 1) {
     return "In your cart, you have " + cart.itemName + " at " + cart.itemPrice + "."
